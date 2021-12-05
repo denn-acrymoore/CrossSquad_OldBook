57a9a17eb11f6be08188e87088f4f1f74aa05301
Audric Lysander <audric.lysander@student.umn.ac.id>
2021-12-05 14:55:04 +0700

    Fix minor bug - Home UI

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,71 +9,71 @@ import './Theme.css';
 const Home: React.FC = () => {
   // Search bar text:
   const [searchText, setSearchText] = useState('');
-  
+
   // Books that don't belong to the user or already in the user's shopping cart:
   const [books, setBooks] = useState<Array<Book>>([]);
   const [booksFirstThree, setBooksFirstThree] = useState<Array<Book>>([]);
   const [booksAfterFirstThree, setBooksAfterFirstThree] = useState<Array<Book>>([]);
 
   const oldBookCtx = useContext(OldBookContext);
-  const {currUser} = oldBookCtx;
+  const { currUser } = oldBookCtx;
   const history = useHistory();
-  
+
   const db = getFirestore(firebaseApp);
 
   const getBooksData = useCallback(() => {
     const unsubscribe = onSnapshot(query(collection(db, "books"), where("bookOwnerUid", "!=", currUser!.uid))
-    , { includeMetadataChanges: true }
-    , (querySnapshot) => {
-      // Check if data is already sent to the server:
-      const source = querySnapshot.metadata.hasPendingWrites ? "Local" : "Server";
-      if (source === "Local") {
-        return;
-      } 
-
-      oldBookCtx.showToast("Fetching books data for home!");
-      const bookList: Array<Book> = [];
-      const bookListFirstThree: Array<Book> = [];
-      const bookListAfterFirstThree: Array<Book> = [];
-      let bookNum = 0;
-
-      querySnapshot.forEach((doc) => {
-        const data = doc.data();
-        const bookData: Book = {
-          bookId: data.bookId,
-          bookOwnerUid: data.bookOwnerUid,
-          bookName: data.bookName,
-          bookDescription: data.bookDescription,
-          bookPrice: data.bookPrice,
-          bookStorageRef: data.bookStorageRef,
-          bookDownloadUrl: data.bookDownloadUrl,
-          bookShoppingCart: data.bookShoppingCart,
-        };
-
-        if (bookData.bookShoppingCart.find(element => element === currUser!.uid)) {
+      , { includeMetadataChanges: true }
+      , (querySnapshot) => {
+        // Check if data is already sent to the server:
+        const source = querySnapshot.metadata.hasPendingWrites ? "Local" : "Server";
+        if (source === "Local") {
           return;
         }
-        else {
-          ++bookNum;
-          bookList.push(bookData);
 
-          if (bookNum <= 3) {
-            bookListFirstThree.push(bookData);
+        oldBookCtx.showToast("Fetching books data for home!");
+        const bookList: Array<Book> = [];
+        const bookListFirstThree: Array<Book> = [];
+        const bookListAfterFirstThree: Array<Book> = [];
+        let bookNum = 0;
+
+        querySnapshot.forEach((doc) => {
+          const data = doc.data();
+          const bookData: Book = {
+            bookId: data.bookId,
+            bookOwnerUid: data.bookOwnerUid,
+            bookName: data.bookName,
+            bookDescription: data.bookDescription,
+            bookPrice: data.bookPrice,
+            bookStorageRef: data.bookStorageRef,
+            bookDownloadUrl: data.bookDownloadUrl,
+            bookShoppingCart: data.bookShoppingCart,
+          };
+
+          if (bookData.bookShoppingCart.find(element => element === currUser!.uid)) {
+            return;
           }
-          else if (bookNum > 3) {
-            bookListAfterFirstThree.push(bookData);
+          else {
+            ++bookNum;
+            bookList.push(bookData);
+
+            if (bookNum <= 3) {
+              bookListFirstThree.push(bookData);
+            }
+            else if (bookNum > 3) {
+              bookListAfterFirstThree.push(bookData);
+            }
           }
-        }
+        });
+
+        setBooks(bookList);
+        setBooksFirstThree(bookListFirstThree);
+        setBooksAfterFirstThree(bookListAfterFirstThree);
       });
-
-      setBooks(bookList);
-      setBooksFirstThree(bookListFirstThree);
-      setBooksAfterFirstThree(bookListAfterFirstThree);
-    });
 
     oldBookCtx.unregisterHomeDataListener = unsubscribe;
   }, []);
-  
+
   // Call getBooksData() once:
   useEffect(() => {
     getBooksData();
@@ -82,14 +82,14 @@ const Home: React.FC = () => {
   const goToProductDetail = (arrayIdx: number, arrayUsed: string) => {
     if (arrayUsed === "booksFirstThree") {
       oldBookCtx.setSelectedBookForShoppingCart(booksFirstThree[arrayIdx]);
-    } 
+    }
     else if (arrayUsed === "booksAfterFirstThree") {
       oldBookCtx.setSelectedBookForShoppingCart(booksAfterFirstThree[arrayIdx]);
     }
     else if (arrayUsed === "books") {
       oldBookCtx.setSelectedBookForShoppingCart(books[arrayIdx]);
     }
-      
+
     history.push("/tabs/productDetail");
   };
 
@@ -104,26 +104,26 @@ const Home: React.FC = () => {
         <IonSearchbar className="searchbar-home" value={searchText} onIonChange={e => setSearchText(e.detail.value!)} animated></IonSearchbar>
 
         {/* Section 1 */}
-        {!searchText && booksFirstThree.length > 0 && 
+        {!searchText && booksFirstThree.length > 0 &&
           <IonCol className="section1">
             <IonCardHeader className="card-home">
               <IonCardTitle>
                 <h3>You will love this books !</h3>
               </IonCardTitle>
             </IonCardHeader>
-            {booksFirstThree.map((book, arrayIdx) => (
-                <IonCardContent className="wrapper card-home">
-                  <IonButton 
-                    onClick={goToProductDetail.bind(null, arrayIdx, "booksFirstThree")}>
-                    <IonImg src={book.bookDownloadUrl} />
-                  </IonButton>
-                </IonCardContent>
-            ))}
+            <IonCardContent className="wrapper card-home">
+              {booksFirstThree.map((book, arrayIdx) => (
+                <IonButton
+                  onClick={goToProductDetail.bind(null, arrayIdx, "booksFirstThree")}>
+                  <IonImg src={book.bookDownloadUrl} />
+                </IonButton>
+              ))}
+            </IonCardContent>
           </IonCol>
         }
 
         {/* Section 2 */}
-        {!searchText && booksAfterFirstThree.length > 0 && 
+        {!searchText && booksAfterFirstThree.length > 0 &&
           <IonGrid className="ion-padding-start ion-padding-end section2">
             <h1>Other Books</h1>
             {booksAfterFirstThree.map((book, arrayIdx) => {
@@ -132,11 +132,11 @@ const Home: React.FC = () => {
                   <IonRow>
                     <IonCol size="6" className="ion-text-center">
                       <IonCard>
-                        <IonButton 
-                          fill="clear" 
-                          className="image-sec2" 
+                        <IonButton
+                          fill="clear"
+                          className="image-sec2"
                           onClick={goToProductDetail.bind(null, arrayIdx,
-                          "booksAfterFirstThree")}
+                            "booksAfterFirstThree")}
                         >
                           <IonImg src={booksAfterFirstThree[arrayIdx].bookDownloadUrl} />
                         </IonButton>
@@ -145,11 +145,11 @@ const Home: React.FC = () => {
                     </IonCol>
                     <IonCol size="6" className="ion-text-center">
                       <IonCard>
-                        <IonButton 
-                          fill="clear" 
-                          className="image-sec2" 
+                        <IonButton
+                          fill="clear"
+                          className="image-sec2"
                           onClick={goToProductDetail.bind(null, arrayIdx + 1,
-                          "booksAfterFirstThree")}
+                            "booksAfterFirstThree")}
                         >
                           <IonImg src={booksAfterFirstThree[arrayIdx + 1].bookDownloadUrl} />
                         </IonButton>
@@ -164,11 +164,11 @@ const Home: React.FC = () => {
                   <IonRow>
                     <IonCol size="6" className="ion-text-center">
                       <IonCard>
-                        <IonButton 
-                          fill="clear" 
-                          className="image-sec2" 
+                        <IonButton
+                          fill="clear"
+                          className="image-sec2"
                           onClick={goToProductDetail.bind(null, arrayIdx,
-                          "booksAfterFirstThree")}
+                            "booksAfterFirstThree")}
                         >
                           <IonImg src={booksAfterFirstThree[arrayIdx].bookDownloadUrl} />
                         </IonButton>
@@ -186,20 +186,20 @@ const Home: React.FC = () => {
         {searchText && books.map((book, arrayIdx) => {
           const searchTextRegex: RegExp = new RegExp(searchText, "i");
 
-          if (searchTextRegex.test(book.bookName) 
-          || searchTextRegex.test(book.bookDescription)) {
+          if (searchTextRegex.test(book.bookName)
+            || searchTextRegex.test(book.bookDescription)) {
             return (
-              <IonCard 
-                className="card-sec3 ion-padding-end" 
+              <IonCard
+                className="card-sec3 ion-padding-end"
                 onClick={goToProductDetail.bind(null, arrayIdx, "books")}
               >
                 <IonCardContent>
                   <IonGrid>
                     <IonRow>
                       <IonCol size="5">
-                        <IonImg 
-                          className="image-sec3" 
-                          src={book.bookDownloadUrl} 
+                        <IonImg
+                          className="image-sec3"
+                          src={book.bookDownloadUrl}
                         />
                       </IonCol>
                       <IonCol size="7">
@@ -214,7 +214,7 @@ const Home: React.FC = () => {
                 </IonCardContent>
               </IonCard>
             );
-          } 
+          }
         })}
       </IonContent>
     </IonPage>
